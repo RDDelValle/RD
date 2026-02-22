3bad34776efaf61f24f90e6c1aa56af11cbb1c55
Ramon Del Valle <ramon.dev@icloud.com>
2026-02-22 04:26:06 -0700

    Add IAnalyticsDbContext interface for EF Core integration

---
+++ Analytics/EntityFrameworkCore/src/IAnalyticsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RD.Analytics;
+
+namespace Rd.Analytics.EntityFrameworkCore;
+
+public interface IAnalyticsDbContext
+{
+    DbSet<AnalyticsUserAgent> UserAgents { get; set; }
+    DbSet<AnalyticsConnection> Connections { get; set; }
+}
